0790c7bd94673c6010a302145243abb4e0190312
Jean Codogno <jeancarlo.eng.comp@gmail.com>
2026-04-27 11:36:13 -0300

    bump: version 0.1.5

--- src/cmd/specforce/main.go
+++ src/cmd/specforce/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/jeancodogno/specforce-kit/src/internal/cli/cobra"
 )
 
-var version = "0.1.4"
+var version = "0.1.5"
 
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
